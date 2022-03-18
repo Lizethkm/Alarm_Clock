d6fd6309df02976d75aa5f312bba3e35c1cabdd1
Lizeth <mendezlizeth084@gmail.com>
2022-03-18 12:50:19 -0700

    fixed calling action names

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from alarm_clock_lab import Alarm_Clock
 current_time= Alarm_Clock()
 current_time.set_time()
 
-toggle_alarm= Alarm_Clock()
-toggle_alarm.turning_alarm_on()
+current_time.turning_alarm_on()
 
-alarm_time=Alarm_Clock()
-alarm_time.setting_alarm_time()
+current_time.setting_alarm_time()
 
